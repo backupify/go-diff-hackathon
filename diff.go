f86e583e2c47ba6d6e8e9974cde9c51f30e33ca3
Brandon Wang <bwang@datto.com>
2016-07-08 11:07:25 -0400

    fixed insert operation

--- diff.go
+++ diff.go
@@ -29,27 +29,35 @@ func check(e error) {
 func main() {
 
 	//read in list of changes here
-	var operations [3]Change
-	//operations[0] = Change{insert, 0, -1, "insert"}
-	operations[0] = Change{delete, 0, 4, ""}
-	//operations[2] = Change{insert, 17, -1, "insert2"}
 
-	fmt.Println(operations)
+	var operations [1]Change
+	//operations[0] = Change{insert, 0, -1, }
+	//operations[0] = Change{delete, 0, 4, ""}
+	//operations[2] = Change{insert, 17, -1, "insert2"}
+	operations[0] = Change{insert, 6, -1, "abc"}
+
+	fmt.Println([]byte("abc"))
 
 	//read in reference text here
-	var referenceBytes, err = ioutil.ReadFile("reference.txt")
+	var referenceBytes, err = ioutil.ReadFile("tmpfile2.txt")
+	fmt.Println(string(referenceBytes))
 	check(err)
 
+	fmt.Println(referenceBytes)
+
 	for _, current := range operations {
+		fmt.Println(current)
 		if current.Operation == insert {
-			referenceBytes = append(append(referenceBytes[:current.Start], []byte(current.Text)...), referenceBytes[current.Start:]...)
+			referenceBytes = append(referenceBytes[:current.Start], append([]byte(current.Text), referenceBytes[current.Start:]...)...)
 		} else if current.Operation == delete {
 			referenceBytes = append(referenceBytes[:current.Start], referenceBytes[current.End+1:]...)
 		}
 	}
 
+	fmt.Println(referenceBytes)
+
 	//dump bytes into file
-	var newFile, err1 = os.Create("new.txt")
+	var newFile, err1 = os.Create("new.bin")
 	check(err1)
 
 	defer newFile.Close()
