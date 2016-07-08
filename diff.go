3365b09b4f937a9e1570c31f57aed0f6c75cd46f
Brandon Wang <bwang@datto.com>
2016-07-08 10:09:18 -0400

    change diff to work with binary data instead of text

--- diff.go
+++ diff.go
@@ -1,63 +1,59 @@
 package main
 
 import (
-    "strings"
-    "os"
-    "fmt"
-    "io/ioutil"
+	"fmt"
+	"io/ioutil"
+	"os"
 )
 
 type ChangeOperation int
 
 const (
-  insert ChangeOperation = iota //insert in front of given character
-  delete ChangeOperation = iota //delete on inclusive range
+	insert ChangeOperation = iota //insert in front of given character
+	delete ChangeOperation = iota //delete on inclusive range
 )
 
 type Change struct {
-    Operation ChangeOperation
-    Start int
-    End int
-    Text string
+	Operation ChangeOperation
+	Start     int
+	End       int
+	Text      string
 }
 
-func main(){
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
-  //read in list of changes here
-  var operations [3]Change
-  //operations[0] = Change{insert, 0, -1, "insert"}
-  operations[0] = Change{delete, 0,4, ""}
-  //operations[2] = Change{insert, 17, -1, "insert2"}
+func main() {
 
-  fmt.Println(operations)
+	//read in list of changes here
+	var operations [3]Change
+	//operations[0] = Change{insert, 0, -1, "insert"}
+	operations[0] = Change{delete, 0, 4, ""}
+	//operations[2] = Change{insert, 17, -1, "insert2"}
 
-  //read in reference text here
-  var referenceBytes, err = ioutil.ReadFile("reference.txt")
-  if err != nil {
-    fmt.Println(err)
-    //log.Fatal(err)
-  }
-  var reference = string(referenceBytes)
+	fmt.Println(operations)
 
-  fmt.Printf("%s", reference)
+	//read in reference text here
+	var referenceBytes, err = ioutil.ReadFile("reference.txt")
+	check(err)
 
-  for _, current := range operations {
-    if current.Operation == insert {
-      reference = strings.Join([]string{reference[:current.Start], current.Text, reference[current.Start:]},"")
-    } else if current.Operation == delete {
-      reference = strings.Join([]string{reference[:current.Start], reference[current.End+1:]},"")
-    }
-  }
+	for _, current := range operations {
+		if current.Operation == insert {
+			referenceBytes = append(append(referenceBytes[:current.Start], []byte(current.Text)...), referenceBytes[current.Start:]...)
+		} else if current.Operation == delete {
+			referenceBytes = append(referenceBytes[:current.Start], referenceBytes[current.End+1:]...)
+		}
+	}
 
-  fmt.Printf("%s", reference)
+	//dump bytes into file
+	var newFile, err1 = os.Create("new.txt")
+	check(err1)
 
-  //save new string
-  var newFile, err1  = os.Create("new.txt")
-  if err1 != nil {
-    fmt.Println(err1)
-    //log.Fatal(err1)
-  }
-  newFile.WriteString(reference)
+	defer newFile.Close()
 
+	newFile.Write(referenceBytes)
 
 }
