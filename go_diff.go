448d389d74e4db623212528453d67fb3c157e13c
Chris Matysik <cmatysik@datto.com>
2016-07-08 15:42:09 -0400

    It's Alive!

--- go_diff.go
+++ go_diff.go
@@ -29,7 +29,7 @@ func computeHash(h uint64, input []byte, oldChar byte, newChar byte) uint64 {
     //uOldChar := uint64(oldChar)
     //uNewChar := uint64(newChar)
 
-    size := SIZE
+    size := len(input)
 
     if h != 0   { // rolling case
         h = (h - uint64(oldChar) * powerfulPow(uint64(PRIME_BASE), size - 1) )// % PRIME_MOD
@@ -70,16 +70,16 @@ func computeRollingHash(input []byte, hmap map[uint64][]int) {
             for j := 0; j < size; j++ {
                 h += uint64(uint64(input[j]) * powerfulPow(uint64(PRIME_BASE), (size - 1) - j))
                 //h = h % PRIME_MOD
-                fmt.Printf("%v", string(input[j]))
+                //fmt.Printf("%v", string(input[j]))
             }
-            fmt.Printf("\n")
+            //fmt.Printf("\n")
 
         }
-        fmt.Printf("%v %v\n", string(oldChar), string(newChar))
+        //fmt.Printf("%v %v\n", string(oldChar), string(newChar))
         oldChar = uint64(input[i])
         if i + size < length    {
             newChar = uint64(input[i + (size)])
-            fmt.Printf("%d %d", i, i + size)
+            //fmt.Printf("%d %d", i, i + size)
         } else  {
             newChar = 0
         }
@@ -88,19 +88,19 @@ func computeRollingHash(input []byte, hmap map[uint64][]int) {
             hmap[h] = make([]int, 0)
         }
         hmap[h] = append(hmap[h], i)
-        fmt.Printf("%v\n", hmap[h])
+        //fmt.Printf("%v\n", hmap[h])
     }
 }
 
 func main() {
     // Read in old file
-    oldBytes, err := ioutil.ReadFile("tmpfile1.txt")
+    oldBytes, err := ioutil.ReadFile("tmpfile2.txt")
 
     if err != nil {
         return
     }
 
-    newBytes, err2 := ioutil.ReadFile("tmpfile2.txt")
+    newBytes, err2 := ioutil.ReadFile("tmpfile1.txt")
 
     if err2 != nil {
         return
@@ -136,25 +136,28 @@ func main() {
             var startRune, endRune byte = 0, 0
             var found bool = false
             var temp []int
-            for (found != true && newPtr < len(newBytes))    {
-                fmt.Printf("%d %d\n", oldPtr, newPtr)
-                fmt.Printf("%d\n", h)
+            for (found != true && newPtr < len(newBytes) && oldPtr < len(oldBytes))    {
+                //fmt.Printf("%d %d ||| %d %d\n", oldPtr, newPtr, len(oldBytes), len(newBytes))
+                //fmt.Printf("%d\n", h)
                 if newPtr + SIZE < len(newBytes)   {
                     newSlice = newBytes[newPtr:newPtr+SIZE]
                 } else {
                     newSlice = newBytes[newPtr:]
                 }
+                //fmt.Printf("%d %v\n", h, string(newSlice))
                 h = computeHash(h, newSlice, startRune, endRune)
                 temp =  hmap[h]
-                fmt.Printf("%v %s\n", temp, newSlice)
-                for i := range temp  {
-                    if i > oldStart {
-                        oldEnd = i
+                //fmt.Printf("%v %s %s %s\n", temp, newSlice, string(startRune), string(endRune))
+                for i := 0; i < len(temp); i++  {
+                    //fmt.Printf("%d %d\n", temp[i], oldStart)
+                    if temp[i] >= oldStart {
+                        oldEnd = temp[i]
                         newEnd = newPtr
                         insertString := oldBytes[oldStart:oldEnd]
 
-                        fmt.Println("Found it!")
+                        //fmt.Println("Found it!")
                         deleteOp := Operation{0, newStart, newEnd, ""}
+                        //fmt.Printf("%d %d %s\n", oldStart, oldEnd, insertString)
                         insertOp := Operation{1, oldStart, oldEnd, string(insertString)}
                         operationList = append(operationList, deleteOp, insertOp)
                         found = true
@@ -165,26 +168,42 @@ func main() {
                 }
                 if !found   {
                     newPtr++
-                    startRune = newSlice[0]
-                    endRune = newSlice[len(newSlice)-1]
+                    oldPtr++
+                    startRune = newBytes[newPtr-1]
+                    if newPtr + SIZE - 1< len(newBytes)   {
+                        endRune = newBytes[newPtr+SIZE-1]
+                    } else  {
+                        endRune = 0
+                    }
+
                 }
             }
        }
 
     }
 
-    fmt.Println("shit didn't break maybe?")
+    //fmt.Println("shit didn't break maybe?")
 
-    fmt.Printf("%v\n", operationList)
     ///////////////////////////////////////////////////////////////////
     // When we reach the end of one file, make a deletion/insertion of the remaining
     // bytes of the other file (operation depending on which has remaining bytes)
     ///////////////////////////////////////////////////////////////////
-    fmt.Printf("%d %d %d\n", oldPtr, newPtr,len(newBytes))
-    // while not EoF
+    //fmt.Printf("%d %d %d\n", oldPtr, newPtr,len(newBytes))
 
-    // compare each byte
+    if oldPtr < len(oldBytes)   {
+        // do an insertion of remaining oldBytes
+        insertOp := Operation{1, oldPtr, len(oldBytes), string(oldBytes[oldPtr:])}
 
-    // set old_s and new_s once a difference occurs
+        fmt.Printf("%x\n", string(newBytes[len(newBytes)-1]))
+        operationList = append(operationList, insertOp)
+    }
+    if newPtr < len(newBytes){
+        // do a deletion of remaining newBytes
+        deleteOp := Operation{0, newPtr, len(newBytes)-1, ""}
+        operationList = append(operationList, deleteOp)
+    }
+
+    fmt.Printf("%v\n", operationList)
+
 }
 
